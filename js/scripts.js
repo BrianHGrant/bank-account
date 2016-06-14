f5f119e269b2025473bd37585e7ef1aa69af05ae
Brian Grant and Benjamin Logue <benjamin@>
2016-06-14 14:34:18 -0700

    add Account object constructor and protype methods to add and withdraw.

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,52 @@
-/*This file is for your custom js.  All yours*/
+// Object constructor for new account
+function Account(name, balance){
+  this.name = name;
+  this.balance = balance;
+}
 
-// Calls input from form-input.html
+Account.prototype.add = function(number){
+  return this.balance += number;
+}
+
+Account.prototype.subtract = function(number) {
+    return this.balance -= number;
+  }
 
 $(document).ready(function(){
 
-  $("#input").submit(function(event){
+  $("#new-account").submit(function(event){
     event.preventDefault();
-    var input = ($("#blank").val());
 
-    var output = "";
+    var accountName = $("#user-name").val();
+    var initialDeposit = parseInt($("#first-deposit").val());
+    if (!accountName) {
+       alert("Please enter a name.");
+    } else if (!initialDeposit) {
+      alert("Please enter a deposit.");
+    } else {
+      newUserAccount = new Account(accountName, initialDeposit);
+    }
 
-    $('#output').text(output);
+    $("#deposit-withdrawal").click(function(){
+      var accountDeposit = parseInt($("#deposit").val());
+      var accountWithdrawal = parseInt($("#withdrawal").val());
+      if (!accountDeposit) {
+         accountDeposit = 0;
+      } else if (!accountWithdrawal) {
+        accountWithdrawal = 0;
+      }
+      newUserAccount.add(accountDeposit);
+      newUserAccount.subtract(accountWithdrawal);
+      $("#account-balance").text(newUserAccount.balance);
+      console.log(newUserAccount.balance)
+      $("#withdrawal").val(0);
+      $("#deposit").val(0);
+    });
+      $("#output").show();
+      $("#account-balance").text(newUserAccount.balance);
 
+    console.log(newUserAccount);
+    // $('#output').text(output);
+    $("#new-account")[0].reset();
   });
 });
